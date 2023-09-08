324b3c3115ca3f9d83db9cf8e9b2007927da7d29
Markus Dethlefsen <git@markus-dethlefsen.de>
2023-09-08 17:04:59 +0200

    remove slint react - we dont need it

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,9 +4,7 @@ module.exports = {
     },
     'extends': [
         'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        // 'plugin:react/recommended',
-        //'plugin:react/jsx-runtime'
+        'plugin:@typescript-eslint/recommended'
     ],
     'overrides': [
         {
